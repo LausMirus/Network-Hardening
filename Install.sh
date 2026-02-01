73c9b7de4db8f9cb31e8e3a602bbe7dd38572c79
LausMirus <nripanofficial@gmail.com>
2026-02-01 08:38:56 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,10 +4,13 @@ source ./wireguard.sh
 
 function requirements(){
     sudo pacman -S wireguard-tools
-    wg_support = lsmod | grep wireguard > ~/Desktop/wg_support.txt
-    if [[ -z ${wg_support} ]];
+    wg_log = lsmod | grep wireguard > ~/Desktop/wg_support_log.txt
+    if [[ -z ${wg_log} ]];
     then
-        echo "system doesn't support wireguard"
+        echo "system doesn't have wireguard module"
         exit
-    else
+    else {
         
+        wg_path = command -v wg && command -v wg-quick >/dev/null 2>&1
+        if [[ ${wg_path} == 1 ]];
+        then
