7fd7ab92f92d760494e45bfdae3990c9c5cc1662
LausMirus <nripanofficial@gmail.com>
2026-02-01 09:30:07 +0530

    Install.sh completed
    Fix syntax errors and improve logging in requirements function.

--- Install.sh
+++ Install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+set -e
 source ./firewall.sh
 source ./wireguard.sh
 
 function requirements(){
-    sudo pacman -S wireguard-tools
-    wg_log = lsmod | grep wireguard > ~/Desktop/wg_support_log.txt
+    sudo pacman -S --noconfirm wireguard-tools
+    wg_log=$(lsmod | grep wireguard | tee ~/Desktop/wg_support_log.txt)
     if [[ -z ${wg_log} ]];
     then
         echo "system doesn't have wireguard module"
-        exit
-    else {
-        
-        wg_path = command -v wg && command -v wg-quick >/dev/null 2>&1
-        if [[ ${wg_path} == 1 ]];
+        exit 1
+    else 
+        wg_path=$(command -v wg)
+        wg_quick_path=$(command -v wg-quick)
+        if [[ -n "${wg_path}" && -n "${wg_quick_path}" ]];
         then
+            echo " wireguard path exist "
+        else 
+            echo " wireguard path doesnt exist in /usr/bin "
+            exit 1
+        fi
+    fi
+}
+requirements
